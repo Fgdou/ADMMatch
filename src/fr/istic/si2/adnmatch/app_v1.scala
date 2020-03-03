102ce43a6d1a7a3090d1b3342f9efec33d421809
Eleven-am <58822198+Eleven-am@users.noreply.github.com>
2020-03-03 14:18:50 +0100

    val seq 2

--- src/fr/istic/si2/adnmatch/app_v1.scala
+++ src/fr/istic/si2/adnmatch/app_v1.scala
@@ -15,4 +15,17 @@ object ADNMatchV1 extends App {
   val seq1 = List(UneBase(A), Choix(UneBase(T), UneBase(A)))
   val seq2 = List(Repete(UneBase(T)), Repete(Nqb), UneBase(G))
   val seq3 = List(Repete(UneBase(T)), NFois(Nqb, 3), UneBase(G))
+
+  //Exo 4
+  /** @param lb une liste de bases azotées
+   *  @return une chaîne de caractères représentant les bases de lb, dans l'ordre */
+  def listeBasesToString(lb: List[Base]): String = {
+    lb match{
+      case Nil => ""
+      case base::remlist => base++listeBasesToString(remlist)
+    }
+  }
 }
+
+
+//Exo 5
