0b88d26c88bd793e7485dd58df11bb13a4267577
Eleven-am <58822198+Eleven-am@users.noreply.github.com>
2020-03-10 16:06:53 +0100

    rexp_basique test sequence image

--- src/fr/istic/si2/adnmatch/sequences_images.scala
+++ src/fr/istic/si2/adnmatch/sequences_images.scala
@@ -16,13 +16,13 @@ object SequencesImages {
   def lignes(lmb: List[(Marqueur, Base)], tligne: Int): List[List[(Marqueur, Base)]] = {
     lmb match {
       case Nil => Nil
-      case _ => returnLigne(lmb, tligne) :: lignes(cutLigne(lmb, tligne), tligne)
+      case _   => returnLigne(lmb, tligne) :: lignes(cutLigne(lmb, tligne), tligne)
     }
   }
 
   /**
    * @param lmb a list of bases with their markers
-   * @param n the length we are working with
+   * @param n   the length we are working with
    * @return a smaller list based on the length of n cut from the beginning
    */
   def returnLigne(lmb: List[(Marqueur, Base)], n: Int): List[(Marqueur, Base)] = {
@@ -39,7 +39,7 @@ object SequencesImages {
 
   /**
    * @param lmb a list of bases with their markers
-   * @param n the length we are working with
+   * @param n   the length we are working with
    * @return a smaller list based on the length of n cut from the end
    */
   def cutLigne(lmb: List[(Marqueur, Base)], n: Int): List[(Marqueur, Base)] = {
@@ -48,7 +48,7 @@ object SequencesImages {
         if (n == 0)
           nlist
         else
-          returnLigne(nlist, n - 1)
+          cutLigne(nlist, n - 1)
       }
       case Nil            => Nil
     }
@@ -65,15 +65,33 @@ object SequencesImages {
    * @param mb une base azotée marquée
    * @return une image représentant la base avec son marqueur visuel
    */
-  // TODO V3
-  def marqueurBaseToImage(mb: (Marqueur, Base)): Image = ???
+  def marqueurBaseToImage(mb: (Marqueur, Base)): Image = {
+    val text = mb._2 match {
+      case A => "A"
+      case T => "T"
+      case G => "G"
+      case C => "C"
+    }
+
+    val img = Text(text, fontSizeBase)
+    val colour = mb match {
+      case (In, _)  => Color(0, 100, 0, 255)
+      case (Out, _) => BLACK
+    }
+
+    LineColor(fillColor(img, colour), colour)
+  }
 
   /**
    * @param ligne une liste de bases azotées marquées
    * @return une image représentant les bases marquées de ligne, dans l'ordre, toutes sur la même ligne
    */
-  // TODO V3
-  def imageUneLigne(ligne: List[(Marqueur, Base)]): Image = ???
+  def imageUneLigne(ligne: List[(Marqueur, Base)]): Image = {
+    ligne match {
+      case Nil           => Empty
+      case base :: nlist => Beside(marqueurBaseToImage(base), imageUneLigne(nlist))
+    }
+  }
 
   /**
    * @param llignes une liste de listes de bases azotées marquées
@@ -81,7 +99,11 @@ object SequencesImages {
    *         Chaque élément de llignes est sur une ligne distincte.
    *         Les lignes sont visualisées les unes en dessous des autres.
    */
-  // TODO V3
-  def imagePlusieursLignes(llignes: List[List[(Marqueur, Base)]]): Image = ???
+  def imagePlusieursLignes(llignes: List[List[(Marqueur, Base)]]): Image = {
+    llignes match {
+      case Nil               => Empty
+      case list :: autrelist => Below(imageUneLigne(list), imagePlusieursLignes(autrelist))
+    }
+  }
 
 }
--- tests/fr/istic/si2/test/adnmatch/testADNMatch.scala
+++ tests/fr/istic/si2/test/adnmatch/testADNMatch.scala
@@ -23,8 +23,25 @@ class ADNMatchTest {
    * A compléter
    */
   @Test
-  def test() { // Veuillez nommer vos tests unitaires convenablement.
-    fail()
+  def testListeBasesToString: Unit = {
+    assertEquals(listeBasesToString(A::Nil), "A")
+    assertEquals(listeBasesToString(Nil), "")
+    assertEquals(listeBasesToString(G::Nil), "G")
+    assertEquals(listeBasesToString(T::Nil), "T")
+    assertEquals(listeBasesToString(C::Nil), "C")
+    assertEquals(listeBasesToString(A::T::G::C::Nil), "ATGC")
   }
 
+  def testrExpToString: Unit ={
+    assertEquals(rExpToString(Impossible),"@")
+    assertEquals(rExpToString(Vide),"%")
+    assertEquals(rExpToString(Nqb),".")
+    assertEquals(rExpToString(NFois(UneBase(A), 6)),"(A){6}")
+    assertEquals(rExpToString(Concat(UneBase(A),Choix(UneBase(A), UneBase(C)))), )
+    assertEquals(rExpToString(Impossible),"@")
+    assertEquals(rExpToString(Impossible),"@")
+    assertEquals(rExpToString(Impossible),"@")
+    assertEquals(rExpToString(Impossible),"@")
+    assertEquals(rExpToString(Repete(Choix(UneBase(A), UneBase(C)))),"((A|C))*")
+  }
 }
