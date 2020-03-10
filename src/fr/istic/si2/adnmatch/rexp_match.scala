11466a779f10dc8173f032b4846f847e0412ec40
Eleven-am <58822198+Eleven-am@users.noreply.github.com>
2020-03-10 07:16:33 +0100

    exo 13

--- src/fr/istic/si2/adnmatch/rexp_match.scala
+++ src/fr/istic/si2/adnmatch/rexp_match.scala
@@ -128,63 +128,63 @@ object RExpMatcher {
 
 
 
-/**
- * @param lb une liste de bases azotées
- * @return la liste des bases de lb, dans l'ordre, marquées pour indiquer
- *         que la totalité de lb n'est pas décrite
- */
-// TODO V2
-def sequenceNonDecrite (lb: List[Base] ): List[(Marqueur, Base)] = {
-  lb match {
-    case Nil           => Nil
-    case base :: nlist => (Out, base) :: sequenceDecrite(nlist)
+  /**
+   * @param lb une liste de bases azotées
+   * @return la liste des bases de lb, dans l'ordre, marquées pour indiquer
+   *         que la totalité de lb n'est pas décrite
+   */
+  // TODO V2
+  def sequenceNonDecrite (lb: List[Base] ): List[(Marqueur, Base)] = {
+    lb match {
+      case Nil           => Nil
+      case base :: nlist => (Out, base) :: sequenceDecrite(nlist)
+    }
   }
-}
 
-/**
- * @param e  une expression régulière
- * @param lb une liste de bases azotées
- * @return s'il existe, le plus petit prefixe de lb qui est décrit par e
- */
-// TODO V2
-def prefixeMatch (e: RExp, lb: List[Base] ): Option[List[Base]] = ???
+  /**
+   * @param e  une expression régulière
+   * @param lb une liste de bases azotées
+   * @return s'il existe, le plus petit prefixe de lb qui est décrit par e
+   */
+  // TODO V2
+  def prefixeMatch (e: RExp, lb: List[Base] ): Option[List[Base]] = ???
 
-/**
- * @param pref une liste de bases azotées *préfixe* de lb
- * @param lb   une liste de bases azotées
- * @return la sous-liste de lb située après le préfixe pref
- */
-// TODO V2
-def suppPrefixe (pref: List[Base], lb: List[Base] ): List[Base] = ???
+  /**
+   * @param pref une liste de bases azotées *préfixe* de lb
+   * @param lb   une liste de bases azotées
+   * @return la sous-liste de lb située après le préfixe pref
+   */
+  // TODO V2
+  def suppPrefixe (pref: List[Base], lb: List[Base] ): List[Base] = ???
 
-/**
- * @param e  une expression régulière
- * @param lb une liste de bases
- * @return une liste  (m1, base1)::...::(mN,baseN)::Nil, qui marque,
- *         base après base, les sous-listes de lb décrites par e.
- *         Les basei sont les bases de lb dans l'ordre.
- */
-// TODO V2
-def tousLesMatchs (e: RExp, lb: List[Base] ): List[(Marqueur, Base)] = ???
+  /**
+   * @param e  une expression régulière
+   * @param lb une liste de bases
+   * @return une liste  (m1, base1)::...::(mN,baseN)::Nil, qui marque,
+   *         base après base, les sous-listes de lb décrites par e.
+   *         Les basei sont les bases de lb dans l'ordre.
+   */
+  // TODO V2
+  def tousLesMatchs (e: RExp, lb: List[Base] ): List[(Marqueur, Base)] = ???
 
-/**
- * @param lbm une liste de bases marquées selon un résultat de recherche
- * @return une description textuelle du résultat pour l'utilisateur
- */
-// TODO V2
-def messageResultat (lbm: List[(Marqueur, Base)] ): String = ???
+  /**
+   * @param lbm une liste de bases marquées selon un résultat de recherche
+   * @return une description textuelle du résultat pour l'utilisateur
+   */
+  // TODO V2
+  def messageResultat (lbm: List[(Marqueur, Base)] ): String = ???
 
-/**
- * @param lb une liste de bases azotées marquées
- * @return liste des mêmes bases que lb, mais où tous les marqueurs indiquent
- *         une non-correspondance
- */
-// TODO V3
-def annulerResultat (lb: List[(Marqueur, Base)] ): List[(Marqueur, Base)] = ???
+  /**
+   * @param lb une liste de bases azotées marquées
+   * @return liste des mêmes bases que lb, mais où tous les marqueurs indiquent
+   *         une non-correspondance
+   */
+  // TODO V3
+  def annulerResultat (lb: List[(Marqueur, Base)] ): List[(Marqueur, Base)] = ???
 
-/**
- * @param lbm une liste de bases azotées marquées
- * @return la liste des bases de lbm dont on a oublié les marqueurs, en conservant l'ordre
- */
-// TODO V3
-def sansMarqueurs (lbm: List[(Marqueur, Base)] ): List[Base] = ???
+  /**
+   * @param lbm une liste de bases azotées marquées
+   * @return la liste des bases de lbm dont on a oublié les marqueurs, en conservant l'ordre
+   */
+  // TODO V3
+  def sansMarqueurs (lbm: List[(Marqueur, Base)] ): List[Base] = ???
